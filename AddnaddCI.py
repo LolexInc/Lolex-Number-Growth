308c4619375474edd8754caf32eb34bff3061ee0
Monkeyboy2805 <lolexinc2805@gmail.com>
2016-09-10 16:27:03 +0100

    Fix Even More Fatal Error
    remember the colons stupid!

--- AddnaddCI.py
+++ AddnaddCI.py
@@ -1,6 +1,6 @@
 currnumber = 0
 nextstep = 1
-while True and currnumber<500000
+while True and currnumber<500000:
     currnumber = currnumber + nextstep
     nextstep = nextstep + 1
     print (currnumber)
