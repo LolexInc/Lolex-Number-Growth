e8e5a15f019e0c2fd50ca66fc9c748f294b492fc
Monkeyboy2805 <lolexinc2805@gmail.com>
2016-09-16 13:56:26 +0100

    Removed stupid subprocess.call loop
    Moved some code into a while True loop

--- Addnadd.py
+++ Addnadd.py
@@ -7,28 +7,25 @@ except(ImportError):
     with io.FileIO("currnumberstore.py","w"):
         pass
     with open ("currnumberstore.py","a") as f: f.write("currnumber = 0\nnextstep = 0")
-    subprocess.call("Addnadd.py", shell =True)
 import currnumberstore
 currnumber = currnumberstore.currnumber
 nextstep = currnumberstore.nextstep
 gone = 0
-while gone != 1000:
-    currnumber = currnumber + nextstep
-    nextstep = nextstep +1
-    gone = gone + 1
-    print (currnumber)
-if gone == 1000 or gone >1000:
-    import os
-    os.remove("currnumberstore.py")
-    with io.FileIO("currnumberstore.py","w"):
-        pass
-    with open ("currnumberstore.py","a") as f: f.write("currnumber = ")
-    with open ("currnumberstore.py","a") as f: f.write(str(currnumber))
-
-    
-
-    with open ("currnumberstore.py","a") as f: f.write("\nnextstep = ")
-    with open ("currnumberstore.py","a") as f: f.write(str(nextstep))
-    subprocess.call("Addnadd.py",shell = True)
+while True:
+    while gone != 1000:
+        currnumber = currnumber + nextstep
+        nextstep = nextstep +1
+        gone = gone + 1
+        print (currnumber)
+    if gone == 1000 or gone >1000:
+        import os
+        os.remove("currnumberstore.py")
+        with io.FileIO("currnumberstore.py","w"):
+            pass
+        with open ("currnumberstore.py","a") as f: f.write("currnumber = ")
+        with open ("currnumberstore.py","a") as f: f.write(str(currnumber))
+        with open ("currnumberstore.py","a") as f: f.write("\nnextstep = ")
+        with open ("currnumberstore.py","a") as f: f.write(str(nextstep))
+        gone = 0
     
     
